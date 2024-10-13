3647acadfbc8f84f39a9a109969c8792aa360176
Jari Sokka <sokkajar@gmail.com>
2024-10-13 16:08:19 +0300

    adjust map colors and add zoom option

--- components/ui/worldMap.tsx
+++ components/ui/worldMap.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
-import geoData from "../../assets/data/countries-50m.json"
+import React from 'react';
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import geoData from "../../assets/data/countries-50m.json";
 import { Tooltip } from "react-tooltip";
 import 'react-tooltip/dist/react-tooltip.css';
 
@@ -11,36 +11,77 @@ interface GeographyProps {
   };
 }
 
-
 const WorldMap = () => {
+  const visitedCountries = [
+    "Finland",
+    "Sweden",
+    "Norway",
+    "Germany",
+    "Belgium",
+    "Hungary",
+    "United States of America",
+    "Spain",
+    "South Africa",
+    "Lesotho",
+    "Canada",
+    "Japan",
+    "Tunisia",
+    "Italy",
+    "Thailand",
+    "Vietnam",
+    "Malaysia",
+    "Russia",
+    "Switzerland",
+    "Slovenia",
+    "Austria",
+    "China",
+    "Greece"
+  ];
+
   return (
     <>
       <ComposableMap>
-        <Geographies geography={geoData}>
-          {({ geographies }: { geographies: GeographyProps[] }) =>
-            geographies.map((geo: GeographyProps) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                style={{
-                  default: { fill: "#e5e5e5", outline: "none" },
-                  hover: { fill: "#334155", outline: "none" },
-                  pressed: { outline: "none" }
-                }}
-                onMouseEnter={(e: React.MouseEvent<SVGPathElement>) => {
-                  const tooltipElement = e.currentTarget;
-                  tooltipElement.setAttribute("data-tooltip-content", geo.properties.name);
-                }}
-                data-tooltip-id="tooltip"
-              />
-            ))
-          }
-        </Geographies>
+        <ZoomableGroup zoom={1} minZoom={1} maxZoom={5}>
+          <Geographies geography={geoData}>
+            {({ geographies }: { geographies: GeographyProps[] }) =>
+              geographies
+                .filter((geo: GeographyProps) => geo.properties.name !== "Antarctica")
+                .map((geo: GeographyProps) => {
+                const isVisited = visitedCountries.includes(geo.properties.name);
+
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    stroke="#e5e5e5"
+                    strokeWidth={0.3}
+                    style={{
+                      default: {
+                        fill: isVisited ? "#334155" : "#000000",
+                        outline: "none"
+                      },
+                      hover: {
+                        fill: isVisited ? "#334155" : "#000000",
+                        outline: "none"
+                      },
+                      pressed: { outline: "none" }
+                    }}
+                    onMouseEnter={(e: React.MouseEvent<SVGPathElement>) => {
+                      const tooltipElement = e.currentTarget;
+                      tooltipElement.setAttribute("data-tooltip-content", geo.properties.name);
+                    }}
+                    data-tooltip-id="tooltip"
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
       </ComposableMap>
 
-      <Tooltip id="tooltip" place="top" style={{ backgroundColor: 'black', color: 'white' }}/>
+      <Tooltip id="tooltip" place="top" style={{ backgroundColor: 'black', color: 'white' }} />
     </>
   );
-}
+};
 
-export default WorldMap
+export default WorldMap;
